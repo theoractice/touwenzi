799bdbf29c6b9e8fdbb9fc9275af5017b55cdc4c
theoractice <theoractice@sina.com>
2015-09-16 05:17:02 +0800

    删除废弃了的功能

--- TWZD.Util/Form1.Designer.cs
+++ TWZD.Util/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -43,16 +42,6 @@
             this.button1.Text = "重置数据库";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(93, 41);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "更新数据";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -70,7 +59,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 5;
-            this.button3.Text = "数据测试";
+            this.button3.Text = "处理数据";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
@@ -81,7 +70,6 @@
             this.ClientSize = new System.Drawing.Size(284, 262);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -92,7 +80,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button3;
     }
--- TWZD.Util/Form1.cs
+++ TWZD.Util/Form1.cs
@@ -118,76 +118,6 @@ namespace TWZD.Util
             });
         }
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            ThreadPool.QueueUserWorkItem(delegate
-            {
-                int val = 0;
-
-                using (System.IO.StreamReader file =
-                    new System.IO.StreamReader(string.Format(@"E:\Work\cv\TWZD\misc\test.csv")))
-                {
-                    while (false == file.EndOfStream)
-                    {
-                        string tmp = file.ReadLine();
-                        MatchCollection matches = Regex.Matches(tmp, "\"(.*?)\"");
-
-                        Phrase item = new Phrase();
-                        item.词语 = matches[2].Groups[1].Value;
-                        item.释义 = matches[6].Groups[1].Value;
-                        item.注音 = matches[4].Groups[1].Value;
-
-                        matches = Regex.Matches(tmp, "hide\":(.*?),");
-                        item.单字位置 = matches[0].Groups[1].Value;
-
-                        int pos = int.Parse(item.单字位置) - 1;
-                        string currentChar = new string(item.词语[pos], 1);
-                        //currentChar = "鼠";
-
-                        if (!File.Exists(string.Format(@"E:\Work\cv\TWZD\misc\strokes\{0}.txt", currentChar)))
-                            continue;
-
-                        using (System.IO.StreamReader sfile =
-                            new System.IO.StreamReader(string.Format(@"E:\Work\cv\TWZD\misc\strokes\{0}.txt", currentChar)))
-                        {
-                            string ssstmp = sfile.ReadToEnd().Replace("\n", "\r\n");
-                            ssstmp = Regex.Match(ssstmp, "STROKEINFO\r\n([\\s\\S]*)ENDSTROKE").Groups[1].Value;
-                            string[] strokes = Regex.Split(ssstmp, "\r\n");
-                            StringBuilder stroketmp = new StringBuilder();
-                            for (int num = 0; num < strokes.Length - 1; num++)
-                            {
-                                string tttt = strokes[num];
-                                if (tttt == "")
-                                    continue;
-                                if (tttt.StartsWith("%"))
-                                    continue;
-                                if (tttt[0] == 'B')
-                                    continue;
-                                if (tttt.StartsWith(""))
-                                    break;
-                                stroketmp.Append(strokes[num]);
-                                stroketmp.Append(Environment.NewLine);
-                            }
-                            ssstmp = stroketmp.ToString();
-                            ssstmp = Regex.Match(stroketmp.ToString(), "(.*)").Value;
-                            if (ssstmp == "")
-                                continue;
-
-                            StrokeOrder so = new StrokeOrder();
-                            so.汉字 = currentChar;
-                            so.笔顺数据 = stroketmp.ToString();
-                            so.备注 = "";
-
-                            _mgr.AddToDB(so);
-                            _mgr.AddToDB(item);
-                        }
-
-                        this.Text = val++.ToString();
-                    }
-                }
-            });
-        }
-
         private void button5_Click(object sender, EventArgs e)
         {
             UserConfig cfg = new UserConfig();
